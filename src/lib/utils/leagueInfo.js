8da250110f2be640740ca36becb7fa6e03cc54c4
ganastor <ganastor@gmail.com>
2022-07-19 08:27:55 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -115,7 +115,7 @@ export const managers = [
        "name": "Eugene Proskurovsky",
        "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "New York", // (optional)
-       "bio": "36 year old male looking to trade, trade and then trade again."
+       "bio": "36 year old male looking to trade, trade and then trade again.",
        "photo": "/managers/eugene.jpeg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2010, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
