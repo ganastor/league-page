5833c53f2a7ec46c7c1ed17df51b4dd6d79fa324
ganastor <ganastor@gmail.com>
2022-08-11 09:36:36 -0500

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -203,11 +203,11 @@
 
         <div class="transactions" bind:this={el} >
             <Transactions masterOffset={left} />
-		<!------MPH CUSTOM ----------->
+		<!------MPH CUSTOM -----------
 		{#if browser}
 <script id="cid0020000321216609869" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 100%;">{"handle":"allelitedynasty","arch":"js","styles":{"a":"CC0000","b":100,"c":"FFFFFF","d":"FFFFFF","k":"CC0000","l":"CC0000","m":"CC0000","n":"FFFFFF","p":"10","q":"CC0000","r":100}}</script>
 {/if}
-		<!-------------------------->
+		-------------------------->
         </div>
     </div>
 </div>
