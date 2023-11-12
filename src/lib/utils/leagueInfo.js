4e417907d7a48ce25b7ca0a886d2f10375bb2735
ganastor <ganastor@gmail.com>
2023-11-12 16:01:58 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "937535256406278144";  // your league ID
+export const leagueID = "917061196975607808";  // your league ID
 export const leagueName = "All Elite Dynasty"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
